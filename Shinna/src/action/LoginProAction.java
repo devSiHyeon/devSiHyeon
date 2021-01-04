1fe92bf072423eb18a69eb1e9c56d8edad14b8a3
ParkSiHyeon <s.hyeon.dev@gmail.com>
2021-01-04 16:27:12 +0900

    로그인

--- Shinna/src/action/LoginProAction.java
+++ Shinna/src/action/LoginProAction.java
@@ -33,7 +33,7 @@ public class LoginProAction implements Action {
 		}else {
 			response.setContentType("text/html;charset=UTF-8");
 			PrintWriter out=response.getWriter();
-			out.println("<script>alert('로그인실패');history.back();</script>");
+			out.println("<script>alert('로그인실패');location.href='./Login.do';</script>");
 		}
 		return forward;
 	}
