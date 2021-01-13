d8df1cefc1ba0883818c39c58e02e196c793a34f
ParkSiHyeon <s.hyeon.dev@gmail.com>
2021-01-13 13:47:07 +0900

    회원탈퇴

--- Shinna/src/svc/JoinDeleteProService.java
+++ Shinna/src/svc/JoinDeleteProService.java
@@ -9,28 +9,36 @@ import java.sql.Connection;
 
 import dao.MemberDAO;
 import vo.MemberBean;
+
 public class JoinDeleteProService {
 
-	public boolean isDelete( String pw,String id) {
+	public boolean isDelete(String pw, String id) {
 		boolean isDelete = false;
-		Connection con = getConnection();
-		MemberDAO memberDAO = MemberDAO.getInstance();
-		memberDAO.setConnection(con);
-		MemberBean member = new MemberBean();
-		member.setId(id);
-		member.setPw(pw);
-		String userId = memberDAO.memberInfoId(member);
-		int isDeleteCount = 0;
-		if(userId != null) isDeleteCount = memberDAO.isDelete(id);
-		
-		if(isDeleteCount > 0 ) {
-			commit(con);
-			isDelete = true;
-		} else {
-			rollback(con);
+		Connection con = null;
+		try {
+			con = getConnection();
+			MemberDAO memberDAO = MemberDAO.getInstance();
+			memberDAO.setConnection(con);
+			MemberBean member = new MemberBean();
+			member.setId(id);
+			member.setPw(pw);
+			String userId = memberDAO.memberInfoId(member);
+			int isDeleteCount = 0;
+			if (userId != null)
+				isDeleteCount = memberDAO.isDelete(id);
+
+			if (isDeleteCount > 0) {
+				commit(con);
+				isDelete = true;
+			} else {
+				rollback(con);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+
+			if(con != null) close(con);
 		}
-		
-		close(con);
 		return isDelete;
 	}
 }
