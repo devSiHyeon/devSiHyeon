4ec9d461ae7c5bc20369164ac1dda9f10d57ccc7
dev.SiHyeon <69497828+devSiHyeon@users.noreply.github.com>
2021-07-15 17:54:19 +0900

    진료안내
    진료안내

---
+++ hospital_therapy.php
@@ -0,0 +1,94 @@
+<? include "hospital_Header.php"; ?>
+<? include "hospital_DB.php"; ?>  
+
+<!-- 상단메뉴 Link -->
+<div class="container mt-5">   
+    <h5><span class="material-icons">library_add_check</span> 재활치료</h5>
+    <div class="text-center">
+        <!-- 목차 -->
+        <div class="container mb-5" style="width:90%;">
+            <!-- 내용1 -->
+            <div class="row" >
+                <div class="col-sm mt-3 mr-3 border border-2 rounded-3"> 
+                    <div class="mt-3 mb-2"><img src="T_1.png"></div>
+                    <div class="fs-4 mb-3">척추 클리닉</div>
+                    <div class="mb-1">목, 허리디스크</div>
+                    <div class="mb-1">척추 협착증</div>
+                    <div class="mb-5">척추 측만증</div>
+                </div>
+                <div class="col-1"></div>
+                <div class="col-sm mt-3 mr-3 border border-2 rounded-3"> 
+                    <div class="mt-3 mb-2"><img src="T_2.png"></div>
+                    <div class="fs-4 mb-3">관절 클리닉</div>
+                    <div class="mb-1">무릎 관절통증</div>
+                    <div class="mb-1">손목·팔꿈치 통증</div>
+                    <div class="mb-5">어깨·오십견</div>
+                </div>
+                <div class="col-1"></div>
+                <div class="col-sm mt-3 mr-3 border border-2 rounded-3"> 
+                    <div class="mt-3 mb-2"><img src="T_3.png"></div>
+                    <div class="fs-4 mb-3">통증 클리닉</div>
+                    <div class="mb-1">일자목·거북목</div>
+                    <div class="mb-5">골반·족부통증</div>
+                </div>
+                
+            </div>
+            <!-- 내용2 -->
+            <div class="row" >
+                <div class="col-sm mt-3 mr-3 border border-2 rounded-3"> 
+                    <div class="mt-3 mb-2"><img src="T_4.png"></div>
+                    <div class="fs-4 mb-3">도수재활치료</div>
+                    <div class="mb-1">도수치료</div>
+                    <div class="mb-1">체형교정클리닉</div>
+                    <div class="mb-1">성장클리닉</div>
+                    <div class="mb-5">수술 후 재활센터</div>
+                </div>
+                <div class="col-1"></div>
+                <div class="col-sm mt-3 mr-3 border border-2 rounded-3"> 
+                    <div class="mt-3 mb-2"><img src="T_5.png"></div>
+                    <div class="fs-4 mb-3">관절 클리닉</div>
+                    <div class="mb-1">체외충격파치료</div>
+                    <div class="mb-1">크라이오냉기치료</div>
+                    <div class="mb-1">고주파치료</div>
+                    <div class="mb-5">자기장치료</div>
+                </div>
+                <div class="col-1"></div>
+                <div class="col-sm mt-3 mr-3 border border-2 rounded-3"> 
+                    <div class="mt-3 mb-2"><img src="T_6.png"></div>
+                    <div class="fs-4 mb-3">교통사고</div>
+                    <div class="mb-1">경추염좌</div>
+                    <div class="mb-1">요추염좌</div>
+                    <div class="mb-1">관절통</div>
+                    <div class="mb-5">두통, 어지럼증</div>
+                </div>
+            </div>    
+            <!-- 내용3 -->
+            <div class="row" >
+                <div class="col-sm mt-3 mr-3 border border-2 rounded-3"> 
+                    <div class="mt-3 mb-2"><img src="T_7.png"></div>
+                    <div class="fs-4 mb-3">작업치료</div>
+                    <div class="mb-1">일상 생활치료</div>
+                    <div class="mb-1">인지치료</div>
+                    <div class="mb-1">성장클리닉</div>
+                    <div class="mb-5">수술 후 재활센터</div>
+                </div>
+                <div class="col-1"></div>
+                <div class="col-sm mt-3 mr-3 border border-2 rounded-3"> 
+                    <div class="mt-3 mb-2"><img src="T_8.png"></div>
+                    <div class="fs-4 mb-3">아동치료</div>
+                    <div class="mb-1">특수작업치료</div>
+                    <div class="mb-1">연하재활 및 연하전기자극치료</div>
+                    <div class="mb-1">인지발달치료</div>
+                    <div class="mb-5">감각통합치료</div>
+                </div>
+                <div class="col-1"></div>
+                <div class="col-sm mt-3 mr-3 border border-2 rounded-3"> 
+                    <div class="mt-3 mb-2"><img src="T_9.png"></div>
+                    <div class="fs-4 mb-5">연하치료</div>
+                </div>
+            </div>
+        </div>    
+    </div>
+
+<? include "hospital_Footer.php"; ?> 
+       
