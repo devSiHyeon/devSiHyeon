2ef315fe4552b4d17d03eb6a29840f58669869e8
dev.SiHyeon <69497828+devSiHyeon@users.noreply.github.com>
2021-07-15 17:53:25 +0900

    hospital_admission (입원안내)
    hospital_admission (입원안내)

---
+++ Hospital/hospital_admission.php
@@ -0,0 +1,78 @@
+<? include "hospital_Header.php"; ?> 
+<? include "hospital_DB.php"; ?> 
+
+<head>  
+    <link href="./hospital_admission.css" rel="stylesheet">
+</head>
+<body>
+<!-- 상단메뉴 Link -->
+    <div class="container mt-5">   
+        <!-- 내용 -->
+        <div>
+            <h5><span class="material-icons">library_add_check</span> 입원절차 안내</h5>
+            <div class="admission padding">
+                <div class="admission_1">입원상담</div>
+                <div class="admission_2">입원검토</div>
+                <div class="admission_3">입원결정</div>
+                <div class="admission_4">병실배정</div>
+                <div class="admission_5">입실</div>
+            </div>
+        </div>
+        
+        <div class="padding">
+            <h6>1. 입원상담</h6>
+            <div class="padding">
+                - 전화 / 방문 / 홈페이지 상담 <br>
+                - 상담실을 통해 <strong>입원가능 여부, 병실, 병원비 등 간단한 상담 후</strong> 방문하시면 됩니다.<br>
+                - 전문의와 진료 상담 후 입원이 결정 됩니다.<br>
+                &nbsp;&nbsp;(의무기록 사본을 제출하셔야 합니다.)<br>
+                - 상병명과 상병코드가 있는 소견서 또는 진단서 지참
+            </div>
+            <h6>2. 입원수속</h6>
+            <div class="padding">
+                - 입원 당일 원무과에 오셔서 접수 및 입원 동의서를 작성해주셔야 합니다.
+                
+            </div>
+            <h6>3. 병실배정 및 입실</h6>
+            <div class="padding">
+                - 본원의 병실은 전체 모든 병실에 공기정화전열기, 시스템에어컨 등 최신식 시설을 갖추고 있으며, 환자의 상태와 병력에 따른 배정이 이루어집니다.
+                
+            </div>
+            <h6>4. 준비하실 사항</h6>
+            <div class="padding">
+                - 의료보험증, 소견서(진단서), 약 처방전 <br>
+                - 세면도구, 개인소지품, 복용하시던 약(처방전 포함), 화장지, 물티슈 등 기본적인 개인물품
+                
+            </div>
+            <h6>5. 입원시 주의사항</h6>
+            <div class="padding">
+                - 환자가 돈이나 귀중품을 소지하여 잃어버리는 일이 없도록 해주십시오. <br>
+                - 면회는 환자분들의 치료와 안정에 방해가 되지 않는 시간으로 제한하고 있습니다. <br>
+                - 면회시 음식은 주치의와 상의 후 부드러운 종류로 준비하여 기도가 막히는 일이 없도록 해주십시오. <br>
+                - 전 병실 및 병원 내에서는 금연입니다. <br>
+                - 외박, 외출시 주치의와 사전에 상의하시기 바랍니다.                        
+            </div>
+        </div>
+
+        <div class="mt-5 mb-5">
+            <h5><span class="material-icons">library_add_check</span> 퇴원절차 안내</h5>
+            <div class="padding">                    
+                <h6>1. 퇴원결정</h6>
+                <div class="padding">
+                    - 담당 전문의가 환자의 상태에 따른 상담 후, 퇴원 여부가 결정됩니다. <br>                   
+                </div>
+                <h6>2. 퇴원수납</h6>
+                <div class="padding">
+                    - 간호사와 원무과를 통해 수납절차를 안내받으신 뒤, 퇴원진료비 계산서를 받아 납부하시면 됩니다. <br>                   
+                </div>
+                <h6>3. 퇴원수속</h6>
+                <div class="padding">
+                    - 수납 후 퇴원영수증을 받아, 간호과에 제출하시고 퇴원약과 주의사항을 듣고 귀가하시면 됩니다. <br> 
+                    &nbsp;&nbsp;(조기 퇴원의 경우 전일 미리 말씀해주셔야 다음날 퇴원수속이 가능합니다.)                  
+                </div>
+            </div>
+        </div>            
+    </div>
+
+<? include "hospital_Footer.php"; ?> 
+
