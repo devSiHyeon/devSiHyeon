9d244efb0df643ad323ecca5156b6675345917e5
dev.SiHyeon <69497828+devSiHyeon@users.noreply.github.com>
2021-07-19 09:37:57 +0900

    admin-재고관리

---
+++ Product.php
@@ -0,0 +1,19 @@
+<? include "./Header.php"; ?>
+        
+<div class="container">
+    <table id="Product" class="table table-bordered">
+        <thead>
+            <tr>
+                <th>NO</th>
+                <th>상품코드</th>
+                <th>상품이름</th>
+                <th>판매가격</th>
+                <th>재고수량</th>
+            </tr>
+        </thead>
+    </table>
+</div>
+
+<script src="./Product.js"></script>
+
+<? include "./Footer.php"; ?>
