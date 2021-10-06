93be2a9ab759b0db7a6d60eefc6bf1a2bb015a3f
dev.SiHyeon <69497828+devSiHyeon@users.noreply.github.com>
2021-10-06 15:17:04 +0900

    color_shop_project (21.10.06 비밀번호 수정)
    color_shop_project (21.10.06 비밀번호 수정)

--- color_shop/DB.php
+++ color_shop/DB.php
@@ -1,7 +1,7 @@
 <?
     error_reporting(E_ALL & ~E_NOTICE); 
     ini_set('display_errors', '1');
-    $db = mysqli_connect ("localhost", "hye0n", "dev1407!", "hye0n");
+    $db = mysqli_connect ("localhost", "hye0n", "dev13579!", "hye0n");
 
     // 공통 함수 호출
     include './Function.php';
