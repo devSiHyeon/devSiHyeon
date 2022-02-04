3504daf36a9c2dbb2bdb27c54258e249b4b37d06
dev.SiHyeon <69497828+devSiHyeon@users.noreply.github.com>
2022-02-04 09:59:26 +0900

    Header

---
+++ hotel/Header.php
@@ -0,0 +1,29 @@
+<?php require_once('./DB.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>hotel</title>
+    <style>
+        .footer{
+            width:100%;
+            height:100px;
+            bottom:0;
+            left:0;
+            position:absolute;
+            background-color:gray;
+            text-align:center;
+            color:white;
+        }
+        a{
+            color:black;
+            background-color:#e0e0e0;
+            text-decoration:none;
+            text-align:center;
+        }
+        a:visited{
+            color:black;
+        }
+    </style>
+</head>
+<body>
