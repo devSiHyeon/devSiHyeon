5009493f151b83355ed4e5e898c17bbd7a457208
dev.SiHyeon <69497828+devSiHyeon@users.noreply.github.com>
2022-02-07 16:13:02 +0900

    idx update

--- hotel/modify.php
+++ hotel/modify.php
@@ -1,7 +1,12 @@
 <?php require_once('./Header.php'); 
 
     $user_id    = $_SESSION['user_id'];
-    $idx      = $_GET['idx'];
+    if(strlen($user_id) > 0 && $user_id == 'admin') {
+        $idx    = $_GET['idx'];
+    } else {
+        $idx    = $_SESSION['idx'];
+    }
+        
 
     // 회원정보
     $sql        = 'SELECT user_id, name, tell, address FROM h_detail AS a LEFT JOIN h_member AS b ON a.member_idx = b.idx WHERE a.idx  = \''.$idx.'\'';
@@ -12,6 +17,7 @@
         ?>
     <b>회원 수정</b>  <a href="./Index.php" style="margin-left:200px;">메인화면</a>
     <form name="modify" action="h_modifyP.php" method="POST">
+        <input type='hidden' name='member_idx' value='<?=$idx?>'>
         아이디 : <?=$row['user_id']?> <br>
         이름 : <input type='text' name='name' value='<?=$row['name']?>'> <br>
         연락처 : <input type='text' name='tell' value='<?=$row['tell']?>'> <br>
@@ -31,7 +37,7 @@
     <b>정보 수정</b><br> <br> 
     <form name="modify" action="h_modifyP.php" method="POST">
         아이디 : <?=$_SESSION['user_id']?> <br>
-        비밀번호 : <input type='password' name='pw' value=''> <br>
+        비밀번호 : <input type='password' name='pw' vale=''> <br>
         이름 : <?=$row['name']?> <br>
         연락처 : <input type='text' name='tell' value='<?=$row['tell']?>'> <br>
         주소 : <input type='text' name='address' value='<?=$row['address']?>'> <br><br>
